e9c26d0bc771a3a2a46417cc00afb51214024cc0
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-06-05 23:46:29 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
+import { useEffect } from 'react';
+import { addBackToTop } from 'vanilla-back-to-top';
+
+import Container from './Container';
+import Footer from './Footer';
+import Header from './Header';
+
+const App = () => {
+  useEffect(() => {
+    addBackToTop({
+      diameter: 40,
+      backgroundColor: '#ff0000',
+      textColor: '#000',
+    });
+  }, []);
+
+  useEffect(() => {
+    pageHeader();
+  });
+
+  useEffect(() => {
+    pageFooter();
+  });
+
+  const pageHeader = () => {
+    const { height: pageHeaderHeight } = document
+      .getElementById('header')
+      .getBoundingClientRect();
+
+    document.body.style.paddingTop = `${pageHeaderHeight}px`;
+  };
+
+  const pageFooter = () => {
+    const { height: pageFooterHeight } = document
+      .getElementById('footer')
+      .getBoundingClientRect();
+
+    document.body.style.paddingBottom = `${pageFooterHeight}px`;
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header />
+
+      <Footer />
+    </Container>
   );
 };
+
+export default App;
