e9d34b553c6fe9933128a13c3ae9b5d61979d69a
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-06-06 22:54:44 +0300

    add api(services)

--- src/services/apiMovies.jsx
+++ src/services/apiMovies.jsx
@@ -13,3 +13,27 @@ async function apiMovies(url = '', config = {}) {
 export function fetchTrending() {
   return apiMovies(`${API_URL}/trending/movie/day?api_key=${API_KEY}`);
 }
+
+export function fetchSearchMovies(query) {
+  return apiMovies(
+    `${API_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`
+  );
+}
+
+export function fetchMovieDetails(movieId) {
+  return apiMovies(
+    `${API_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
+}
+
+export function fetchMovieCredits(movieId) {
+  return apiMovies(
+    `${API_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+  );
+}
+
+export function fetchMovieReviews(movieId) {
+  return apiMovies(
+    `${API_URL}/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
+}
